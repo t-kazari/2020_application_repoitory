af5a6682ae199db12d2f39adfd7033449cb5484a
t-kazari <kinno.prime@gmail.com>
2020-06-21 00:36:46 +0900

    一部変更

--- food-composer-server/src/main/java/jp/co/fcserver/util/AuthUtils.java
+++ food-composer-server/src/main/java/jp/co/fcserver/util/AuthUtils.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 
 import com.google.api.client.util.Strings;
 import com.google.auth.oauth2.GoogleCredentials;
@@ -21,6 +22,12 @@ public class AuthUtils {
 
 	private static final String BASE_URL = "https://identitytoolkit.googleapis.com/v1/accounts";
 	private static final Logger LOG = LoggerFactory.getLogger(AuthUtils.class);
+
+	@Value("${firebase.auth.service.path}")
+	private static String fileName;
+
+	@Value("${firebase.auth.service.backet}")
+	private static String backetName;
 
 	public static boolean isAuthUser(String token) throws ComposerException {
 
@@ -47,7 +54,7 @@ public class AuthUtils {
 
 		FirebaseApp firebaseApp = null;
 		List<FirebaseApp> firebaseApps = FirebaseApp.getApps();
-		InputStream credentialsStream = GcpUtils.getGcsFile("fcservice", "fire/food-composer-front-firebase-adminsdk-r2k3d-767ee04adc.json");
+		InputStream credentialsStream = GcpUtils.getGcsFile(backetName, fileName);
 		if (firebaseApps != null && !firebaseApps.isEmpty()) {
 			for (FirebaseApp app : firebaseApps) {
 				if (app.getName().equals(FirebaseApp.DEFAULT_APP_NAME)) {
